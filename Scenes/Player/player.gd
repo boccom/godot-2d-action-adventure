5456471be0157a7e25946b0e2ded4e3167545e09
Bojan M. <204991996+boccom@users.noreply.github.com>
2025-06-24 21:16:20 +0200

    Nove stvari - iz kursa

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+func _ready(): 
+	var score: int = 200
+	
+	var money: float = 63.5
+	
+	var is_player_alive: bool = true
+	
+	print (score + 200 * score + 100 * 30 + score + 30000 * score * 1,000,000)
+	print (first_name)
+	print (damage)
+	var username: String = "Bob"
+	
+	print (username)
+	print (money + 100,000)
+
+var first_name: String = "Bojan"
+
+var damage: float = 7.5
